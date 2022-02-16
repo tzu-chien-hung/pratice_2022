59e62af8fbed6969a38511a275b2d1bc1d6ac895
tina <tina19960522@gmail.com>
2022-02-16 23:28:01 +0800

    ajax REST API TEST

---
+++ data/test.js
@@ -0,0 +1,14 @@
+// let name1 = 'tina'
+
+// let person ={
+//     name:"tina",
+//     age: 25,
+//     id: 5
+// }
+
+let img = {
+    "title": "圖片樣式",
+    "src" : "圖片連結",
+    "nref" : "點擊連結",
+    "target" : "目標項目"
+}
